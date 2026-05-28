bb508614dab24a0f5a4477c7044a46127a77b0f5
Alvin00111 <wmawma905@gmail.com>
2026-05-28 16:45:24 +0530

    added chat rendering logic

--- js/script.js
+++ js/script.js
@@ -16,15 +16,15 @@ class SidebarComponent {
     constructor() {
         this.chatListContainer = document.querySelector('.chat-row-list')
     }
-    chatListRender(chatData, chatId = 1) {
+    chatListRender(chatData, chatId, onChatSelect) {
 
-        chatData.sort((chatA, chatB)=>{
+        chatData.sort((chatA, chatB) => {
             const lastMsgA = chatA.messages[chatA.messages.length - 1];
             const lastMsgB = chatB.messages[chatB.messages.length - 1];
-            
+
             const timeA = lastMsgA ? lastMsgA.timestamp : 0;
             const timeB = lastMsgB ? lastMsgB.timestamp : 0;
-            
+
             return timeB - timeA;
         })
 
@@ -69,6 +69,10 @@ class SidebarComponent {
                         </div>
                     </div>
             `
+             chatItem.addEventListener("click", () => {
+                onChatSelect(item.id);
+            });
+
             fragment.appendChild(chatItem);
         });
 
@@ -77,18 +81,63 @@ class SidebarComponent {
     }
 }
 
+class ChatWindowComponent {
+    constructor() {
+        this.messageFeed = document.querySelector(".message-feed")
+        this.chatInput = document.querySelector(".chat-input")
+        this.headerName = document.querySelector(".chat-window-username")
+        this.headerAvatar = document.querySelector(".chat-window-img")
+    }
+    updateHeader(contact) {
+        this.headerName.textContent = contact.name
+        this.headerAvatar = contact.avatar
+    }
+
+    renderFeed(contact) {
+        this.messageFeed.innerHTML = ""
+        const fragment = document.createDocumentFragment();
+
+        contact.messages.forEach(item => {
+            const msgRow = document.createElement("div")
+            msgRow.className = `${item.type === "received" ? "msg-row msg-row--received" : "msg-row msg-row--sent"}`
+            msgRow.innerHTML = `
+            <div class="msg-bubble">
+                        <p class="msg-text">${item.text}</p>
+                        <div class="msg-meta">
+                            <span class="msg-time">${item.time}</span>
+                            ${item.type === "sent" ? `
+                    <svg viewBox="0 0 18 18" height="18" width="18" preserveAspectRatio="xMidYMid meet"
+                        class="status-check" version="1.1" x="0px" y="0px" enable-background="new 0 0 18 18">
+                        <title>status-dblcheck</title>
+                        <path
+                            d="M17.394,5.035l-0.57-0.444c-0.188-0.147-0.462-0.113-0.609,0.076l-6.39,8.198 c-0.147,0.188-0.406,0.206-0.577,0.039l-0.427-0.388c-0.171-0.167-0.431-0.15-0.578,0.038L7.792,13.13 c-0.147,0.188-0.128,0.478,0.043,0.645l1.575,1.51c0.171,0.167,0.43,0.149,0.577-0.039l7.483-9.602 C17.616,5.456,17.582,5.182,17.394,5.035z M12.502,5.035l-0.57-0.444c-0.188-0.147-0.462-0.113-0.609,0.076l-6.39,8.198 c-0.147,0.188-0.406,0.206-0.577,0.039l-2.614-2.556c-0.171-0.167-0.447-0.164-0.614,0.007l-0.505,0.516 c-0.167,0.171-0.164,0.447,0.007,0.614l3.887,3.8c0.171,0.167,0.43,0.149,0.577-0.039l7.483-9.602 C12.724,5.456,12.69,5.182,12.502,5.035z">
+                        </path>
+                    </svg>` : ""}
+                        </div>
+                    </div>
+                    `
+            fragment.appendChild(msgRow)
+
+        })
+        this.messageFeed.appendChild(fragment);
+
+    }
+
+}
+
 
 class WhatsAppApplication {
     constructor() {
         this.chatsPool = []
-        console.log(new Date(Date.now() - 50000))
+        this.activeChatId = null
+        this.chatWindow = new ChatWindowComponent()
         this.sidebar = new SidebarComponent()
 
     }
 
     init() {
         this.loadData()
-        this.sidebar.chatListRender(this.chatsPool)
+        this.renderAll()
     }
 
     loadData() {
@@ -104,8 +153,8 @@ class WhatsAppApplication {
                     { id: 1, text: "Hii", time: "10:24 pm", timestamp: Date.now() - 50000, type: "received" },
                     { id: 2, text: "Hello", time: "10:26 pm", timestamp: Date.now() - 45000, type: "sent" },
                     { id: 3, text: "Hi", time: "10:28 pm", timestamp: Date.now() - 40040, type: "received" },
-                    { id: 4, text: "Hi, Good Morning", time: "10:30 pm", timestamp: Date.now() - 35060, type: "sent" },
-                    { id: 5, text: "Hii sir, Good Morning", time: "10:32 pm", timestamp:Date.now() - (32000), type: "received" }
+                    { id: 4, text: "Hi, Good Morning", time: "10:30 pm", timestamp: Date.now() - 35060, type: "received" },
+                    { id: 5, text: "Hii sir, Good Morning", time: "10:32 pm", timestamp: Date.now() - (32000), type: "sent" }
                 ]
             },
             {
@@ -115,11 +164,23 @@ class WhatsAppApplication {
                     { id: 1, text: "Hii", time: "09:24 pm", timestamp: Date.now() - 50000, type: "received" },
                     { id: 2, text: "Hii", time: "09:26 pm", timestamp: Date.now() - 45000, type: "sent" },
                     { id: 3, text: "Hello", time: "09:28 pm", timestamp: Date.now() - 40040, type: "received" },
-                    { id: 4, text: "submit your notes", time: "09:30 pm", timestamp: Date.now() - 35060, type: "sent" },
-                    { id: 5, text: "Ok", time: "09:32 pm", timestamp: Date.now() - 33070, type: "received" }
+                    { id: 4, text: "submit your notes", time: "09:30 pm", timestamp: Date.now() - 35060, type: "received" },
+                    { id: 5, text: "Ok", time: "09:32 pm", timestamp: Date.now() - 33070, type: "sent" }
                 ]
             }]
         }
+    }
+
+    handleChatSelection(chatId) {
+        this.activeChatId = chatId;
+        this.renderAll();
+    }
+
+    renderAll() {
+        const activeChat = this.chatsPool.find(c => c.id === this.activeChatId) || this.chatsPool[0];
+        this.sidebar.chatListRender(this.chatsPool, activeChat.id, this.handleChatSelection.bind(this))
+        this.chatWindow.renderFeed(activeChat)
+        this.chatWindow.updateHeader(activeChat);
     }
 }
 
